dcd1fa6d238af4fc784e9fad175eb5439773d9b1
Leo Sell <leosell14@gmail.com>
2023-01-14 23:15:52 -0300

    tentando ajustar a rota para receber a requisição do frontend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import router from "./routes/routes.js";
 dotenv.config({path: './config/config.env'})
 
 const server = express()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 const type_port = process.env.NODE_ENV
 
 server.listen(
